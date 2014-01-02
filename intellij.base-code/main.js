d7fdbd588a67c679ea576f0e2d669fa25c6f55e6
Colt Fortenberry <colt.fortenberry@outlook.com>
2014-01-02 00:50:32 -0500

    v1.0 Release - Working

--- intellij.base-code/main.js
+++ intellij.base-code/main.js
@@ -1,33 +1,33 @@
 define(function (require, exports, module) {
 	"use strict";
 	
-	var CommandManager = brackets.getModule("command/CommandManager");
-	Menus = brackets.getModule("command/Menus");
+	var CommandManager = brackets.getModule("command/CommandManager"),
+		EditorManager = brackets.getModule("editor/EditorManager"),
+		FileSystem = brackets.getModule("filesystem/FileSystem"),
+		FileUtils = brackets.getModule("file/FileUtils"),
+		Menus = brackets.getModule("command/Menus");
 	
-	var file = FileSystem.getFileForPath('base.txt');
+	var file = FileSystem.getFileForPath(require.toUrl('./html_template.txt'));
 	var promise = FileUtils.readAsText(file);
 	var baseCode = '';
-	promise.done(function(text) {
-		console.log('Loaded base code contents. Ready for usage.');
+	promise.done(function (text) {
 		baseCode = text;
 	});
 	
-	
-	
-	function handleInsertBaseCode() {
+	function insertBaseCode() {
 		var editor = EditorManager.getFocusedEditor();
 		if (editor) {
 			if (baseCode != '') {
-				editor.replace(baseCode);
+				editor.document.replaceRange(baseCode, editor.getCursorPos());
 			}
 		}
 	}
 	
 	var INSERT_BASE_CODE = "basecode.insert";
-	CommandManager.register("Insert Base Code", INSERT_BASE_CODE, handleInsertBaseCode);
+	CommandManager.register("Insert Base Code", INSERT_BASE_CODE, insertBaseCode);
 	
-	var menu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
-	menu.addMenuItem(INERT_BASE_CODE, "Ctrl-Shift-I");
+	var menu = Menus.addMenu("IntelliJ", "intellij.menu");
+	menu.addMenuItem(INSERT_BASE_CODE, "Ctrl-Shift-I");
 	
-	exports.handleInsertBaseCode = handleInsertBaseCode;
+	exports.handleInsertBaseCode = insertBaseCode;
 });
